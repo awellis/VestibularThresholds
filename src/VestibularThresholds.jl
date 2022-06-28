8a23ed01838b4fdf6724572845f1a9e2bdc8d7df
Andrew Ellis <andrewellis@pm.me>
2022-06-28 16:08:14 +0200

    Update VestibularThresholds.jl

--- src/VestibularThresholds.jl
+++ src/VestibularThresholds.jl
@@ -1,11 +1,16 @@
 module VestibularThresholds
 
-using Turing, Random
+using Random, Distributions
+using DataFrames
+using Turing
 using CairoMakie
 using Base: @kwdef
+using StatsFuns: logistic
 
+export ModelParams
 export MixtureModelParams, PsychophysicsModelParams
-export simulate
+export bernoulli_mixture, simulate
+export mixture_model, psychophysics_model, logistic_regression
 
 include("utils.jl")
 include("generate-data.jl")
