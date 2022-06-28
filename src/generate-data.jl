18da61c590fc1e3298d2177cee1531d02d430c94
Andrew Ellis <andrewellis@pm.me>
2022-06-28 19:30:55 +0200

    Update generate-data.jl

--- src/generate-data.jl
+++ src/generate-data.jl
@@ -60,10 +60,3 @@ simulate(params::MixtureModelParams,
                 nreps::Int64=10, noise::Bool=false) = simulate(params, collect(stimulus_levels); 
                 nreps=nreps, noise=noise)
 
-
-function fit_model(model::Function, d::DataFrame; alg=NUTS())
-	m = model(d.x, d.y)
-	alg = NUTS()
-	fit = Turing.sample(m, alg, MCMCThreads(), 1000, 4)
-end
-
